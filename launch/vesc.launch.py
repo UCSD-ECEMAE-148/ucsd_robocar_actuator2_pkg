1035b47c086bde8a4e56e30a4eed1c2fa30966d2
Dominic Nightingale <djnighti@ucsd.edu>
2021-10-07 03:59:22 +0000

    Update vesc.launch.py

--- launch/vesc.launch.py
+++ launch/vesc.launch.py
@@ -5,7 +5,7 @@ from launch_ros.actions import Node
 
 
 def generate_launch_description():
-    pkg = 'ucsd_robocar_actuator_pkg'
+    pkg = 'ucsd_robocar_actuator2_pkg'
     steering_node_name = 'vesc_steering_node.py'
     throttle_node_name = 'vesc_rpm_node.py'
 
