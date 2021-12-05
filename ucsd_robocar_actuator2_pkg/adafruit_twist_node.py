aced71cf37cc08a3eeb91efc838c4e883be882e9
Dominic Nightingale <djnighti@ucsd.edu>
2021-12-05 01:15:10 +0000

    Update adafruit_twist_node.py

--- ucsd_robocar_actuator2_pkg/adafruit_twist_node.py
+++ ucsd_robocar_actuator2_pkg/adafruit_twist_node.py
@@ -1,7 +1,6 @@
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
-from .vesc_submodule.vesc_client import VESC_
 
 NODE_NAME = 'adafruit_twist_node'
 TOPIC_NAME = '/cmd_vel'
