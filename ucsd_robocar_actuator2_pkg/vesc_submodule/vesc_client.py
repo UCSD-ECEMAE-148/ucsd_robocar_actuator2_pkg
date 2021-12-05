6e2a9cada2f095e8357476fca41b62cc8ec127a4
Dominic Nightingale <djnighti@ucsd.edu>
2021-12-05 01:51:33 +0000

    Update vesc_client.py

--- ucsd_robocar_actuator2_pkg/vesc_submodule/vesc_client.py
+++ ucsd_robocar_actuator2_pkg/vesc_submodule/vesc_client.py
@@ -14,8 +14,12 @@ class VESC_:
         self.has_sensor = False
         self.start_heartbeat = True
         print("Connecting to VESC")
-        self.v = VESC(self.serial_port, self.baudrate, self.has_sensor, self.start_heartbeat)
-        print("VESC Connected")
+        try:
+            self.v = VESC(self.serial_port, self.baudrate, self.has_sensor, self.start_heartbeat)
+            print("VESC Connected")
+        except FileNotFoundError:
+        print("Could not connect to VESC")
+
         self.send_rpm(0)
         self.inverted = -1 if self.is_inverted else 1
 
