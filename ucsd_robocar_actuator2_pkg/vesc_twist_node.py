25350e2dc1b3c29e560ab83a0d4ad2256f052f1d
Dominic Nightingale <djnighti@ucsd.edu>
2021-12-05 22:18:42 +0000

    Update vesc_twist_node.py

--- ucsd_robocar_actuator2_pkg/vesc_twist_node.py
+++ ucsd_robocar_actuator2_pkg/vesc_twist_node.py
@@ -15,16 +15,24 @@ class VescTwist(Node):
         self.rpm_subscriber = self.create_subscription(Twist, TOPIC_NAME, self.callback, 10)
 
         # Default actuator values
-        self.default_rpm_value = int(5000)
+        self.default_rpm_value = int(5000) 
+        self.default_steering_polarity = int(1) # if polarity is flipped, switch from 1 --> -1
+        self.default_throttle_polarity = int(1) # if polarity is flipped, switch from 1 --> -1
         self.declare_parameters(
             namespace='',
             parameters=[
-                ('max_rpm', self.default_rpm_value)
+                ('max_rpm', self.default_rpm_value),
+                ('steering_polarity', self.default_steering_polarity),
+                ('throttle_polarity', self.default_throttle_polarity)
             ])
-        self.max_rpm = self.get_parameter('max_rpm').value
+        self.max_rpm = int(self.get_parameter('max_rpm').value)
+        self.steering_polarity = int(self.get_parameter('steering_polarity').value)
+        self.throttle_polarity = int(self.get_parameter('throttle_polarity').value)
 
     def callback(self, msg):
-        self.max_rpm = self.get_parameter('max_rpm').value
+        self.max_rpm = self.get_parameter('max_rpm').value # ability to update max RPM in real-time
+        self.steering_polarity = self.get_parameter('steering_polarity').value # ability to update steering polarity in real-time
+        self.throttle_polarity = self.get_parameter('throttle_polarity').value # ability to update throttle polarity in real-time
         # Steering map from [-1,1] --> [0,1]  
         vesc_min_limit = 0
         vesc_max_limit = 1
@@ -33,10 +41,10 @@ class VescTwist(Node):
         steering_angle = float(-0.1 + ((msg.angular.z-data_min_limit)*(vesc_max_limit - vesc_min_limit))/(data_max_limit-data_min_limit))
         
         # RPM map from [-1,1] --> [-max_rpm,max_rpm]
-        rpm = int(self.max_rpm * msg.linear.x)
+        rpm = self.max_rpm * msg.linear.x
 
-        self.vesc.send_rpm(rpm)
-        self.vesc.send_servo_angle(steering_angle)
+        self.vesc.send_rpm(int(self.throttle_polarity * rpm))
+        self.vesc.send_servo_angle(float(self.steering_polarity * steering_angle))
 
 
 def main(args=None):
