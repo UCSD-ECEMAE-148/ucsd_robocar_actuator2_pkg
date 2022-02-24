e0c3cc68819feb92bf731dc7728913751e3f2313
Dominic Nightingale <djnighti@ucsd.edu>
2022-02-24 20:49:36 +0000

    Update ucsd_robocar_actuator2_pkg/vesc_submodule/__init__.py, ucsd_robocar_actuator2_pkg/__init__.py, ucsd_robocar_actuator2_pkg/adafruit_servo_node.py

---
+++ ucsd_robocar_actuator2_pkg/adafruit_servo_node.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import Float32
+from adafruit_servokit import ServoKit
+
+NODE_NAME = 'adafruit_servo_node'
+TOPIC_NAME = 'adafruit_servo'
+
+'''
+[0, 180]degrees: [full right, full left]
+'''
+
+class AdafruitServo(Node):
+    def __init__(self):
+        super().__init__(NODE_NAME)
+        self.steering_subscriber = self.create_subscription(Float32, TOPIC_NAME, self.callback, 10)
+        self.kit = ServoKit(channels=16)
+        self.bus_num = 1
+        self.servo_channel = 1
+        if self.bus_num == 0:
+            i2c_bus0 = (busio.I2C(board.SCL_1, board.SDA_1))
+            self.kit = ServoKit(channels=16, i2c=i2c_bus0)
+        else:
+            self.kit = ServoKit(channels=16)
+
+    def callback(self, data):
+        kit.servo[self.servo_channel].angle = data.data
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    adafruit_servo = AdafruitServo()
+    try:
+        rclpy.spin(adafruit_servo)
+        adafruit_servo.destroy_node()
+        rclpy.shutdown()
+    except KeyboardInterrupt:
+        adafruit_servo.get_logger().info(f'Shutting down {NODE_NAME}...')
+        adafruit_servo.destroy_node()
+        rclpy.shutdown()
+        adafruit_servo.get_logger().info(f'{NODE_NAME} shut down successfully.')
+
+
+if __name__ == '__main__':
+    main()
