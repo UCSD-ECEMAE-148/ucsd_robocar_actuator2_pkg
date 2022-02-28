269b20a45b2f9d2f1b6de6dea2a1924abbd52349
Dominic Nightingale <djnighti@ucsd.edu>
2022-02-28 03:41:41 +0000

    Update ucsd_robocar_actuator2_pkg/vesc_twist_node.py

--- ucsd_robocar_actuator2_pkg/vesc_twist_node.py
+++ ucsd_robocar_actuator2_pkg/vesc_twist_node.py
@@ -17,23 +17,35 @@ class VescTwist(Node):
         self.default_rpm_value = int(10000) 
         self.default_steering_polarity = int(1) # if polarity is flipped, switch from 1 --> -1
         self.default_throttle_polarity = int(1) # if polarity is flipped, switch from 1 --> -1
-        self.steering_offset = -0.1
+        self.default_max_right_steering = 0.8
+        self.default_straight_steering = 0.4
+        self.default_max_right_steering = 0.1
         self.declare_parameters(
             namespace='',
             parameters=[
                 ('max_rpm', self.default_rpm_value),
                 ('steering_polarity', self.default_steering_polarity),
-                ('throttle_polarity', self.default_throttle_polarity)
+                ('throttle_polarity', self.default_throttle_polarity),
+                ('max_right_steering', self.default_max_right_steering),
+                ('straight_steering', self.default_straight_steering),
+                ('max_left_steering', self.default_max_right_steering)
             ])
         self.max_rpm = int(self.get_parameter('max_rpm').value)
         self.steering_polarity = int(self.get_parameter('steering_polarity').value)
         self.throttle_polarity = int(self.get_parameter('throttle_polarity').value)
+        self.max_right_steering = self.get_parameter('max_right_steering').value
+        self.straight_steering = self.get_parameter('straight_steering').value
+        self.max_left_steering = self.get_parameter('max_left_steering').value
 
         self.get_logger().info(
             f'\nmax_rpm: {self.max_rpm}'
             f'\nsteering_polarity: {self.steering_polarity}'
             f'\nthrottle_polarity: {self.throttle_polarity}'
+            f'\nmax_right_steering: {self.max_right_steering}'
+            f'\nstraight_steering: {self.straight_steering}'
+            f'\nmax_left_steering: {self.max_left_steering}'
             )
+        self.steering_offset = 0.5 - self.remap(self.straight_steering)
 
 
     def callback(self, msg):
@@ -45,12 +57,13 @@ class VescTwist(Node):
         #     f'\nsteering_polarity: {self.steering_polarity}'
         #     f'\nthrottle_polarity: {self.throttle_polarity}'
         #     )
-        # Steering map from [-1,1] --> [0,1]  
-        data_min_limit = -1
-        data_max_limit = 1 
-        vesc_min_limit = 0 # These will be rosparams eventually... : max_left
-        vesc_max_limit = 1 # These will be rosparams eventually... : max_right
-        steering_angle = float(self.steering_offset + ((msg.angular.z-data_min_limit)*(vesc_max_limit - vesc_min_limit))/(data_max_limit-data_min_limit))
+        # # Steering map from [-1,1] --> [0,1]  
+        # data_min_limit = -1
+        # data_max_limit = 1 
+        # vesc_min_limit = 0 # These will be rosparams eventually... : max_left
+        # vesc_max_limit = 1 # These will be rosparams eventually... : max_right msg.angular.z
+        # steering_angle = float(self.steering_offset + ((msg.angular.z-data_min_limit) * (vesc_max_limit - vesc_min_limit)) / (data_max_limit-data_min_limit))
+        steering_angle = float(self.steering_offset + self.remap(msg.angular.z))
         
         # RPM map from [-1,1] --> [-max_rpm,max_rpm]
         rpm = int(self.max_rpm * msg.linear.x)
@@ -58,6 +71,15 @@ class VescTwist(Node):
 
         self.vesc.send_rpm(int(self.throttle_polarity * rpm))
         self.vesc.send_servo_angle(float(self.steering_polarity * steering_angle))
+
+        def remap(self, value):
+            input_start = -1
+            input_end = 1
+            output_start = 0
+            output_end = 1
+            normalized_output = float(output_start + (value - input_start) * ((output_end - output_start) / (input_end - input_start)))
+            return normalized_output
+
 
 
 def main(args=None):
